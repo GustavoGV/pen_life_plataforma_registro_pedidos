9def1804ceeb7404cca3c4aed289e4eb3865466d
Gustavo Gomes Andrade <gustavo.g.a@icloud.com>
2021-11-07 20:05:42 -0300

    init

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import Steps from './components/steps';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Steps></Steps>
+      
+    </div>
+  );
+}
+
+export default App;
